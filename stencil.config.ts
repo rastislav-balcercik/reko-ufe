86d04fac6e71d2bfa77309a93515d1a176aeac26
Rastislav Balcerčík <rasto.balcercik@gmail.com>
2024-03-23 22:56:37 +0100

    Global imports

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,6 +2,7 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'reko-ufe',
+  globalScript: 'src/global/app.ts',
   outputTargets: [
     {
       type: 'dist',
